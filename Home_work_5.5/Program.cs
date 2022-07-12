9116520ba73943df6fdec3cc25bb0e66ba81e7f7
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-12 23:16:24 +0700

    Задача №36 в 5.5.2 - выполнена!

--- Home_work_5.5/Program.cs
+++ Home_work_5.5/Program.cs
@@ -15,7 +15,7 @@ namespace Home_work_5._5
             int n = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[n];
             FillArray(array);
-
+            Console.WriteLine($"Сумма чисел в нечетных индексах массива: [{String.Join(", ", array)}] равна: {FuncSumOddNumbers(array)}");
 
             void FillArray(int[] arrayInFunction)
             {
@@ -23,7 +23,16 @@ namespace Home_work_5._5
                     arrayInFunction[i] = new Random().Next(1, 100);
             }
 
-
+            int FuncSumOddNumbers(int[] arrayInFunction)
+            {
+                int sumOddNumbers = 0;
+                for (int i = 0; i < arrayInFunction.Length; i++)
+                {
+                    if (i % 2 == 1)
+                        sumOddNumbers += arrayInFunction[i];
+                }
+                return (sumOddNumbers);
+            }
         }
     }
 }
