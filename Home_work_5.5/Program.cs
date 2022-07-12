b263f948f76e1e7021ea414cd0f211435e44d8f2
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-12 23:20:18 +0700

    Начало работы с дз 5.5.3 - Задача 38

--- Home_work_5.5/Program.cs
+++ Home_work_5.5/Program.cs
@@ -6,33 +6,10 @@ namespace Home_work_5._5
     {
         private static void Main(string[] args)
         {
-            // Задача 36: Задайте одномерный массив, заполненный случайными числами.
-            // Найдите сумму элементов, стоящих на нечётных позициях.
-            // [3, 7, 23, 12] -> 19
-            // [-4, -6, 89, 6]-> 0
+            // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+            // [3 7 22 2 78] -> 76
 
-            Console.Write("Введите размерность массива: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[n];
-            FillArray(array);
-            Console.WriteLine($"Сумма чисел в нечетных индексах массива: [{String.Join(", ", array)}] равна: {FuncSumOddNumbers(array)}");
 
-            void FillArray(int[] arrayInFunction)
-            {
-                for (int i = 0; i < arrayInFunction.Length; i++)
-                    arrayInFunction[i] = new Random().Next(1, 100);
-            }
-
-            int FuncSumOddNumbers(int[] arrayInFunction)
-            {
-                int sumOddNumbers = 0;
-                for (int i = 0; i < arrayInFunction.Length; i++)
-                {
-                    if (i % 2 == 1)
-                        sumOddNumbers += arrayInFunction[i];
-                }
-                return (sumOddNumbers);
-            }
         }
     }
 }
