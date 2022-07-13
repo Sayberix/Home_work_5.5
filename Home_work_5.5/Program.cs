08b10d55e3aa10eed665c7dfd95220ec7054cc2c
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-13 22:30:53 +0700

    Добавлена функция FillArray - которая заполняет массив случайными числами

--- Home_work_5.5/Program.cs
+++ Home_work_5.5/Program.cs
@@ -9,6 +9,18 @@ namespace Home_work_5._5
             // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
             // [3 7 22 2 78] -> 76
 
+            Console.Write("Введите размерность массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] array = new double[n];
+            FillArray(array);
+            Console.WriteLine($"Разница между максимальным и минимальным элементом в массиве: [{String.Join(", ", array)}] равна: {DiffMinMax(array)}");
+
+            void FillArray(double[] arrayInFunction)
+            {
+                for (int i = 0; i < arrayInFunction.Length; i++)
+                    arrayInFunction[i] = new Random().Next(1, 100);
+            }
+
 
         }
     }
